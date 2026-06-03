8660bd5cfe73ab87ed1471b466988db7307da75c
Lihu0 <lihu.levit@gmail.com>
2026-06-03 14:18:25 +0300

    Add initial layout for Year in Pixels app

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<div
+  class="flex h-screen w-screen items-center justify-center bg-stone-200 p-4"
+>
+  <main
+    class="text-graphite flex w-full max-w-150 flex-col bg-white p-4 shadow-[0_20px_40px_rgba(0,0,0,0.12)] outline outline-black/5 sm:p-8"
+  >
+    <h1
+      class="font-handwritten-cursive mb-4 shrink-0 text-center text-4xl sm:text-6xl"
+    >
+      Year in Pixels
+    </h1>
+    <div
+      class="font-handwritten-sans flex min-h-0 flex-1 flex-col items-center gap-4 sm:flex-row sm:justify-between"
+    >
+      <div>Value table</div>
+      <div
+        class="flex flex-col items-center justify-center gap-4 w-fit"
+      >
+        <p>Colors select</p>
+        <p>Year select</p>
+      </div>
+    </div>
+  </main>
+</div>
