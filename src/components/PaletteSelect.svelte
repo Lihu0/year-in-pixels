7fdd57fbb2b2f003b633bca8d8efe0909e3e841f
Lihu0 <lihu.levit@gmail.com>
2026-06-08 13:23:57 +0300

    Update PaletteSelect to have options for label and color change

--- src/components/PaletteSelect.svelte
+++ src/components/PaletteSelect.svelte
@@ -7,16 +7,23 @@
 >
   {#each palette as paletteValue}
     <div class="flex items-center gap-2">
-      <div
-        class="border-text size-6 border-2"
+      <label
+        class="border-text size-6 cursor-pointer border-2"
         style:background={paletteValue.color}
-      ></div>
+      >
+        <input
+          type="color"
+          class="absolute inset-0 cursor-pointer opacity-0"
+          bind:value={paletteValue.color}
+        />
+      </label>
 
       <span
+        role="textbox"
+        contenteditable
+        bind:innerText={paletteValue.label}
         class="py-1.5 text-sm underline underline-offset-[3px] sm:text-base"
-      >
-        {paletteValue.label}
-      </span>
+      ></span>
     </div>
   {/each}
 </div>
