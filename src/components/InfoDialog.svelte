38a0d07e0afca46368f6f88b53095514f1ac92e1
Lihu0 <lihu.levit@gmail.com>
2026-06-10 20:15:34 +0300

    Add InfoDialog component with placeholder text

---
+++ src/components/InfoDialog.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import X from "@lucide/svelte/icons/x";
+</script>
+
+<dialog
+  id="info-dialog"
+  class="fixed inset-0 m-0 p-5 bg-white w-full sm:w-100 h-screen max-h-none max-w-none shadow-xl backdrop:bg-black/33 backdrop:backdrop-blur-[2px]"
+>
+  <div class="flex justify-end">
+    <button
+      class="cursor-pointer text-gray-400 hover:text-gray-600"
+      commandfor="info-dialog"
+      command="close"><X /></button
+    >
+  </div>
+
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam alias,
+    natus quia suscipit odio, nisi laborum saepe rerum, repudiandae voluptatibus
+    corporis autem dolor facere dignissimos.
+  </p>
+</dialog>
--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -7,6 +7,8 @@
   import ImageDown from "@lucide/svelte/icons/image-down";
   import Info from "@lucide/svelte/icons/info";
 
+  import InfoDialog from "./InfoDialog.svelte";
+
   import { entries, palette } from "../lib/state.svelte";
 
   async function downloadImage() {
@@ -96,10 +98,11 @@
 
 <div class="flex w-full justify-end gap-2">
   <button
-    class="cursor-not-allowed opacity-50"
-    disabled
+    class="cursor-pointer"
     title="Information (WIP)"
     aria-label="Information (WIP)"
+    command="show-modal"
+    commandfor="info-dialog"
   >
     <Info />
   </button>
@@ -137,3 +140,5 @@
     <FileDown />
   </button>
 </div>
+
+<InfoDialog />
